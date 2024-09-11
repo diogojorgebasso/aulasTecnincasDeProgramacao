8eeaa709ec4a500c9b47118092deb3f780f6d73f
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2024-09-11 03:20:02 -0400

    Delete OBI 2022 directory

--- OBI 2022/Fase Local/ex1.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-soma = int(input())
-lista = [int(i) for i in input().split()]
-output = 0
-for x in range(0, len(lista)):
-    for y in range(x,len(lista)+1):
-        if sum(lista[x:y]) == soma:
-            output+=1
-print(output)
